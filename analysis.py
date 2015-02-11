c4e207baaf9f6b3ad9e5a770f0995d0b120b3f2e
Tiny-Box <Tiny_Box@outlook.com>
2015-02-12 02:31:12 +0800

    class

--- analysis.py
+++ analysis.py
@@ -5,10 +5,15 @@ import jieba.posseg as pseg
 
 class person(object):
 	"""docstring for person"""
+
+
 	def __init__(self, arg):
-		super(person, self).__init__()
-		self.articlelist = os.listdir(arg)
-		
+		for x in flag_f:
+			dic[x] = []
+		self.articlelist = [ arg + "\\" + x for x in os.listdir(arg)]
+		for i in self.articlelist:
+			with open(i) as f:
+				self.corpus.append(f.read())
 
 fileaddress = "D:\\wya"
 # fileaddress2 = "D:\\llr"
@@ -24,22 +29,26 @@ flag = ['j', 'ad', 'p', 'zg', 'vn', 'an', 'eng', 'nz', 'mq', 'nr', 'ns', 'nt', '
 flag_s = ['n','nt','nd','nl','nh','nhh','nhf','nhg','nhy','nhr','nhw','ns','nn','ni','nz','v','vu','vd','vl','aq','as','f','m','q','d','r','p','c','u','e','o','i','in','iv','ia','ic','j','jn','jv','ja','h','k','g','x','w','wp','ws','wu']
 flag_f = ['j', 'ad', 'p', 'zg', 'vn', 'an', 'eng', 'nz', 'mq', 'nr', 'ns', 'nt', 'v', 'a', 'c', 'b', 'd', 'f', 'i', 'k', 'uv', 'm', 'l', 'n', 'q', 'ul', 's', 'r', 'u', 't', 'uj', 'y', 'ud', 'ug', 'z', 'x', 'nrt', 'o', 'uz', 'df', 'ng', 'vq', 'e', 'nrfg', 'rz', 'vi', 'h', 'vg', 'rr', 'ag', 'tg', 'vd', 'g', 'dg', 'yg', 'rg', 'mg']
 
-for x in flag_f:
-	dic[x] = []
-	sta[x] = 0
+testone = person(fileaddress)
 
-for x in flist:
-	with open(fileaddress + "\\" + x) as f:
-		data = f.read()
-		words = pseg.cut(data)
-		for w in words:
-			if w.flag != "x":
-				dic[w.flag].append(w.word)
-				sta[w.flag] += 1
+print testone.articlelist
+
+# for x in flag_f:
+# 	dic[x] = []
+# 	sta[x] = 0
+
+# for x in flist:
+# 	with open(fileaddress + "\\" + x) as f:
+# 		data = f.read()
+# 		words = pseg.cut(data)
+# 		for w in words:
+# 			if w.flag != "x":
+# 				dic[w.flag].append(w.word)
+# 				sta[w.flag] += 1
 
 
-for k in sta:
-	print k + " is ", sta[k]
+# for k in sta:
+# 	print k + " is ", sta[k]
 
 
 
